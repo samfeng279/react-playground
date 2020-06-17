bc723bd3a7312f3f06dadc79e8f1534e1ecac2aa
Sam Feng <samfeng279@gmail.com>
2020-06-17 13:50:20 -0400

    create-react-app to set up app, first hook

---
+++ react-app/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Example() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Example />
+      </div>
+    );
+  }
+}
+
+export default App;
